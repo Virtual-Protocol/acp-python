045bb3dfb943da96e9319af9c74224496644e005
StevenSF1998 <steven@virtuals.io>
2025-11-07 16:11:35 +0800

    add v2 main net config

--- virtuals_acp/configs/configs.py
+++ virtuals_acp/configs/configs.py
@@ -119,7 +119,7 @@ BASE_MAINNET_CONFIG_V2 = ACPContractConfig(
 
 BASE_MAINNET_ACP_X402_CONFIG = ACPContractConfig(
     chain="base",
-    rpc_url="https://mainnet.base.org",
+    rpc_url="https://alchemy-proxy-prod.virtuals.io/api/proxy/rpc",
     chain_id=8453,
     contract_address="0x6a1FE26D54ab0d3E1e3168f2e0c0cDa5cC0A0A4A",
     base_fare=Fare("0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913", 6),
@@ -127,6 +127,21 @@ BASE_MAINNET_ACP_X402_CONFIG = ACPContractConfig(
     alchemy_policy_id="186aaa4a-5f57-4156-83fb-e456365a8820",
     acp_api_url="https://acpx.virtuals.io/api",
     abi=ACP_X402_ABI,
+    x402_config=X402Config(
+        url="https://acp-x402.virtuals.io",
+    ),
+)
+
+BASE_MAINNET_ACP_X402_CONFIG = ACPContractConfig(
+    chain="base",
+    rpc_url="https://alchemy-proxy-prod.virtuals.io/api/proxy/rpc",
+    chain_id=8453,
+    contract_address="0xa6C9BA866992cfD7fd6460ba912bfa405adA9df0",
+    base_fare=Fare("0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913", 6),
+    alchemy_base_url="https://alchemy-proxy.virtuals.io/api/proxy/wallet",
+    alchemy_policy_id="186aaa4a-5f57-4156-83fb-e456365a8820",
+    acp_api_url="https://acpx.virtuals.io/api",
+    abi=ACP_V2_X402_ABI,
     x402_config=X402Config(
         url="https://acp-x402.virtuals.io",
     ),
